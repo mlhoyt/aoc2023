0d386481f750b3b5b21eeca67d98eb3160f3526c
Mike Hoyt <derukimlh@gmail.com>
2023-12-01 18:29:40 -0800

    add rs lib utils

---
+++ rs/src/lib.rs
@@ -0,0 +1,13 @@
+// pub mod grid2d;
+
+use std::io::{BufRead, BufReader};
+
+pub fn read_stdin() -> Result<String, std::io::Error> {
+    let file = std::io::stdin();
+    let mut file = BufReader::new(file);
+
+    let mut input = String::new();
+    while file.read_line(&mut input)? > 0 {}
+
+    Ok(input)
+}
